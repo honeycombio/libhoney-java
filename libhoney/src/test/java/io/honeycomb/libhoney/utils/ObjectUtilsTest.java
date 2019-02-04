0493a3ad694ef4faa0520fc2b0b463e21ecde6e0
Alaina Valenzuela <asdvalenzuela@gmail.com>
2019-02-04 11:24:56 -0800

    add assertions for year, month, day, and hour

--- libhoney/src/test/java/io/honeycomb/libhoney/utils/ObjectUtilsTest.java
+++ libhoney/src/test/java/io/honeycomb/libhoney/utils/ObjectUtilsTest.java
@@ -12,21 +12,28 @@ public class ObjectUtilsTest {
 
     @Test
     public void checkThatTheFormatterCanHandleZuluTime() throws ParseException {
-        final Date parse = ObjectUtils.getRFC3339DateTimeFormatter().parse("2018-05-01T12:01:00.925Z");
+        final Date parse = ObjectUtils.getRFC3339DateTimeFormatter().parse("2018-05-01T12:01:43.925Z");
 
+        assertThat(parse).hasYear(2018);
+        assertThat(parse).hasMonth(05);
+        assertThat(parse).hasDayOfMonth(1);
+        assertThat(parse).hasHourOfDay(5);
         assertThat(parse).hasMinute(01); 
-        assertThat(parse).hasSecond(00);
+        assertThat(parse).hasSecond(43);
         assertThat(parse).hasMillisecond(925);
-        assertThat(parse).isNotNull();
+
     }
 
     @Test
     public void checkThatTheFormatterCanHandleZonedTime() throws ParseException {
-        final Date parse = ObjectUtils.getRFC3339DateTimeFormatter().parse("2018-05-01T12:01:00.925+01:00");
+        final Date parse = ObjectUtils.getRFC3339DateTimeFormatter().parse("2018-05-01T12:01:43.925+01:00");
 
+        assertThat(parse).hasYear(2018);
+        assertThat(parse).hasMonth(05);
+        assertThat(parse).hasDayOfMonth(1);
+        assertThat(parse).hasHourOfDay(4);
         assertThat(parse).hasMinute(01); 
-        assertThat(parse).hasSecond(00);
+        assertThat(parse).hasSecond(43);
         assertThat(parse).hasMillisecond(925);
-        assertThat(parse).isNotNull();
     }
 }
