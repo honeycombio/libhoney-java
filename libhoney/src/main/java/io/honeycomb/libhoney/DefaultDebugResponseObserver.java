9bff4bda36daf66ae80c6b8964311929f9970ed5
Ramit Prasad <rprasad@wundermancommerce.com>
2020-05-13 20:40:27 -0700

    Honey-11 Added log message for DefaultDebugResponseObserver when api host cannot be contacted or proxy configuration is invalid.

--- libhoney/src/main/java/io/honeycomb/libhoney/DefaultDebugResponseObserver.java
+++ libhoney/src/main/java/io/honeycomb/libhoney/DefaultDebugResponseObserver.java
@@ -7,12 +7,17 @@ import io.honeycomb.libhoney.responses.Unknown;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.UnknownHostException;
+
 public class DefaultDebugResponseObserver implements ResponseObserver {
     private static final Logger LOG = LoggerFactory.getLogger(DefaultDebugResponseObserver.class);
 
     protected static final String ERROR_TEMPLATE_401 = "Server responded with a 401 HTTP error code to a batch request." +
         " This is likely caused by using an incorrect 'Team Write Key'. Check https://ui.honeycomb.io/account to verify your " +
         "team write key. Rejected event: {}";
+    protected static final String ERROR_TEMPLATE_UNKNOWN_HOST_EXCEPTION = "Could not reach determine destination server. " +
+        "This could be due to 1) invalid apiHost 2) Could not resolve apiHost (firewall? dns?) 3) problem with proxy configuration. " +
+        "Unknown event: {}";
     private static final int STATUS_UNAUTHORIZED = 401;
 
     @Override
@@ -36,7 +41,11 @@ public class DefaultDebugResponseObserver implements ResponseObserver {
 
     @Override
     public void onUnknown(final Unknown unknown) {
-        LOG.debug("Received an unknown error while trying to send Event to Honeycomb: {}", unknown);
+        if(unknown.getException()!=null && UnknownHostException.class.isAssignableFrom(unknown.getException().getClass())){
+            LOG.debug(ERROR_TEMPLATE_UNKNOWN_HOST_EXCEPTION, unknown);
+        } else {
+            LOG.debug("Received an unknown error while trying to send Event to Honeycomb: {}", unknown);
+        }
     }
 
     protected void handle401(final ServerRejected serverRejected) {
